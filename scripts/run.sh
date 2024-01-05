c9ae0ab0ddc644fbb344ab4f594b7bf831770be7
Aditya Ranjan <adi.k.ranjan@gmail.com>
2024-01-05 12:34:00 -0500

    using args from repo

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,7 +4,11 @@ source /nfshomes/aranjan2/adi-venv/bin/activate
 
 export PYTHONPATH="${PYTHONPATH}:/nfshomes/aranjan2/adi-venv/improved-diffusion"
 
-cmd="python image_train.py &> out.txt &" 
+MODEL_FLAGS="--image_size 32 --num_channels 128 --num_res_blocks 3 --learn_sigma True --dropout 0.3"
+DIFFUSION_FLAGS="--diffusion_steps 4000 --noise_schedule cosine"
+TRAIN_FLAGS="--lr 1e-4 --batch_size 128"
+
+cmd="python image_train.py $MODEL_FLAGS $DIFFUSION_FLAGS $TRAIN_FLAGS &> out.txt &" 
 
 echo "${cmd}"
 

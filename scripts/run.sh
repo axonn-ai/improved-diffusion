2e131c676d2ea6299bb57a6d125a78ea881b4495
Aditya Ranjan <adi.k.ranjan@gmail.com>
2024-01-05 12:43:22 -0500

    replicating args with serial run

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,7 +6,12 @@ module load mpi/mpich-x86_64
 
 export PYTHONPATH="${PYTHONPATH}:/nfshomes/aranjan2/adi-venv/improved-diffusion"
 
-cmd="mpirun -np 2 python image_train.py &> out.txt &" 
+# divided batch size (128) by number of gpus (2) to get 64
+MODEL_FLAGS="--image_size 32 --num_channels 128 --num_res_blocks 3 --learn_sigma True --dropout 0.3"
+DIFFUSION_FLAGS="--diffusion_steps 4000 --noise_schedule cosine"
+TRAIN_FLAGS="--lr 1e-4 --batch_size 64"
+
+cmd="mpirun -np 2 python image_train.py $MODEL_FLAGS $DIFFUSION_FLAGS $TRAIN_FLAGS &> out.txt &" 
 
 echo "${cmd}"
 

3df6f143dc2aab7c9dd93e73363a96dbb77ef20c
Aditya Ranjan <adityaranjan@login15.frontier.olcf.ornl.gov>
2024-01-25 01:51:03 -0500

    stuff

--- scripts/run.sh
+++ scripts/run.sh
@@ -9,11 +9,12 @@
 
 source /ccs/home/adityaranjan/scratch/my-venv/bin/activate
 module load amd/5.6.0
+module load libfabric
 
 ## these lines enable CUDA aware MPI
-# module load craype-accel-amd-gfx90a
-# export MPICH_GPU_SUPPORT_ENABLED=0
-# export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${CRAY_MPICH_ROOTDIR}/gtl/lib"
+module load craype-accel-amd-gfx90a
+export MPICH_GPU_SUPPORT_ENABLED=0
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${CRAY_MPICH_ROOTDIR}/gtl/lib"
 
 ## this enables the slingshot-11 plugin for RCCL (crucial for inter-node bw)
 # export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/lustre/orion/scratch/adityaranjan/csc547/my-venv/aws-ofi-rccl/build/lib"
@@ -26,6 +27,11 @@ module load amd/5.6.0
 # export CUDA_DEVICE_MAX_CONNECTIONS=1
 
 export PYTHONPATH="${PYTHONPATH}:/ccs/home/adityaranjan/scratch/my-venv/improved-diffusion"
+
+export MIOPEN_USER_DB_PATH="/tmp/my-miopen-cache"
+export MIOPEN_CUSTOM_CACHE_DIR=${MIOPEN_USER_DB_PATH}
+rm -rf ${MIOPEN_USER_DB_PATH}
+mkdir -p ${MIOPEN_USER_DB_PATH}
 
 # divided batch size (128) by num gpus (2) to get 64 batch size
 MODEL_FLAGS="--image_size 32 --num_channels 128 --num_res_blocks 3 --learn_sigma True --dropout 0.3"

75c77ef44b70aabe6427540a42d7b166f380f75d
Aditya Tomar <aditya26042005@gmail.com>
2024-05-20 16:37:03 -0400

    add load samples script

---
+++ scripts/load_samples.py
@@ -0,0 +1,23 @@
+import os
+import numpy as np
+from PIL import Image
+
+def main():
+    npz_path = "/ccs/home/adityatomar/improved-diffusion/samples/samples_1000x64x64x3.npz"
+    output_path = "/ccs/homes/adityatomar/improved-diffusion/samples/sample_images"
+
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    with np.load(npz_path) as f:
+        arr = f["arr_0"]
+
+    print(f"num samples: {arr.shape[0]}")
+    print(f"dims: {arr.shape[1]}x{arr.shape[2]}x{arr.shape[3]}")
+
+    for i in range(arr.shape[0]):
+        im = Image.fromarray(arr[i])
+        im.save(output_path + f"sample_{i}.png")
+
+if __name__ == "__main__":
+    main()
